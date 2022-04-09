0a7134dd1358b51ea5ffae98fbe8c31cc54bd00f
Allen Clarke <a.clarke17@outlook.com>
2022-04-09 16:29:21 +0100

    Added day6 code

---
+++ Day6 - No Coding/main.py
@@ -0,0 +1,3 @@
+# No actual code was writting on VSCode or Replit 
+# But pratice code to move the mini aroud on
+# link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
